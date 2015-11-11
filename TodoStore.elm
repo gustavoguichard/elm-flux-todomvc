be472572a3deeecbc1d16960cc865c44f437000e
Gustavo Guichard <gustavoguichard@gmail.com>
2015-11-11 02:04:37 -0800

    Wiring up ports between JS and Elm

--- TodoStore.elm
+++ TodoStore.elm
@@ -1,3 +1,55 @@
 module TodoStore where
 
+
+type Action
+  = Create String
+  | Complete TodoId
+  | Destroy TodoId
+  | DestroyCompleted
+  | ToggleCompleteAll
+  | UndoComplete TodoId
+  | UpdateText TodoId String
+
+
+type alias TodoItem =
+  { id : String }
+
+
+type alias TodoId = Int
+
+
+actions =
+  Signal.mergeMany
+    [ Signal.map Create dispatchCreate
+    , Signal.map Complete dispatchComplete
+    , Signal.map Destroy dispatchDestroy
+    , Signal.map (always DestroyCompleted) dispatchDestroyCompleted
+    , Signal.map (always ToggleCompleteAll) dispatchToggleCompleteAll
+    , Signal.map UndoComplete dispatchUndoComplete
+    , Signal.map (\(id, text) -> UpdateText id text) dispatchUpdateText ]
+
+
 port dispatchCreate : Signal String
+port dispatchComplete : Signal TodoId
+port dispatchDestroy : Signal TodoId
+port dispatchDestroyCompleted : Signal ()
+port dispatchToggleCompleteAll : Signal ()
+port dispatchUndoComplete : Signal TodoId
+port dispatchUpdateText : Signal (TodoId, String)
+
+
+update action model =
+  model
+
+
+initialModel =
+  { todos = []
+  , uid = 1 }
+
+
+modelChanges =
+  Signal.foldp update initialModel actions
+
+port todoListChanges : Signal (List TodoItem)
+port todoListChanges = Signal.map .todos modelChanges
+                        |> Signal.dropRepeats
--- js/stores/TodoStore.js
+++ js/stores/TodoStore.js
@@ -116,6 +116,32 @@ var TodoStore = assign({}, EventEmitter.prototype, {
   }
 });
 
+var defaultValues = {
+  dispatchCreate: "",
+  dispatchComplete: 0,
+  dispatchDestroy: 0,
+  dispatchDestroyCompleted: [],
+  dispatchToggleCompleteAll: [],
+  dispatchUndoComplete: 0,
+  dispatchUpdateText: [0, ""]
+};
+
+var ports = Elm.worker(Elm.TodoStore, defaultValues).ports;
+
+ports.todoListChanges.subscribe(function(updatedTodoList) {
+  // Convert from the flat list we're using in Elm
+  // to the keyed-by-id object the JS code expects.
+  _todos = {};
+
+  updatedTodoList.forEach(function(item) {
+    _todos[item.id] = item;
+  });
+
+  TodoStore.emitChange();
+});
+
+ports.dispatchCreate.send("Gotta port more JS to Elm!");
+
 // Register callback to handle all updates
 AppDispatcher.register(function(action) {
   var text;
